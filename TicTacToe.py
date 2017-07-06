0426d440ede6796ba94591f0276893ddaf807f6f
worldMachine <kbdavis77@gmail.com>
2017-07-06 14:59:05 -0700

    Fixed indentation issues, deleted useless comments.

--- TicTacToe.py
+++ TicTacToe.py
@@ -4,23 +4,23 @@ import random
 person = input('Enter your name: ')
 print('Hello', person)
 
-field = [[1, 2, 3],[4, 5, 6],[7, 8, 9]]
+field = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
 
 def printField():
-    counter = 0;
+    counter = 0
 
     for i in field[:2]:
         for j in i[:2]:
-            print(" ", j, " | ", end="")
+            print (" ", j, " | ", end="")
         print(" ", field[counter][2])
         counter += 1
         print ("------------------")
-    print (" ", field[2][0], " |  ", field [2][1], " |  ", field[2][2])
-counter = 0;
+    print (" ", field[2][0], " |  ", field[2][1], " |  ", field[2][2])
 
-#print (field [1][1]) #is 5
-#printField()
-#move = input ('Choose your move: ')
+
+counter = 0
+
 
 def switch(x):
     return {
@@ -35,11 +35,11 @@ def switch(x):
         '9': [2, 2]
     }[x]
 
+
 def populate(x, side):
     nums = switch(x)
     field[nums[0]][nums[1]] = side
 
-#populate(move, 'x')
 
 winnerPos = [["1", "2", "3"], ["1", "4", "7"], ["1", "5", "9"], ["4", "5", "6"], ["7", "8", "9"], ["2", "5", "8"], ["3", "6", "9"], ["3", "5", "7"]]
 
@@ -51,161 +51,144 @@ thirdAdvantage = [["1", "3", "5"], ["3", "5", "9"], ["7", "5", "9"], ["1", "5", 
 
 
 def think():
-  if moveCounter == 0:
-    answer = "5"
-  elif moveCounter == 1:
-    if userMoves[-1] == "5":
-      answer = "1"
+    if moveCounter == 0:
+        answer = "5"
+    elif moveCounter == 1:
+        if userMoves[-1] == "5":
+            answer = "1"
+        else:
+            answer = "5"
     else:
-      answer = "5"
-  else:
-    answer = anticipateWin()
+        answer = anticipateWin()
+        if answer == "0":
+            answer = anticipateUserWin()
+        if answer == "0":
+            answer = anticipateAdvantage()
+        if answer == "0":
+            answer = anticipateUserAdvantage()
     if answer == "0":
-      answer = anticipateUserWin()
-    if answer == "0":
-      answer = anticipateAdvantage()
-    if answer == "0":
-      answer = anticipateUserAdvantage()
-  if answer == "0":
-    answer = random.choice(moves)
-  myMoves.append(answer)
-  return answer
+        answer = random.choice(moves)
+    myMoves.append(answer)
+    return answer
+
 
 def anticipateWin():
-  answer = "0"
-  for lis in winnerPos:
-    commonEl = set(myMoves) & set(lis)
+    answer = "0"
+    for lis in winnerPos:
+        commonEl = set(myMoves) & set(lis)
 
-    #for i in myMoves:
-     # print("my moves: ", i)
+        if len(commonEl) > 1:
+            for el in lis:
+                if el not in commonEl:
+                    if el in moves:
+                        answer = el
+                        break
+                        break
+    return answer
 
-    #for i in lis:
-     # print("list item #", i)
-
-    #for i in commonEl:
-     # print ("common: ", i)
-
-    if len(commonEl) > 1 :
-      for el in lis:
-        if el not in commonEl:
-          if el in moves:
-            answer = el
-            break
-            break
-
-  return answer
 
 def anticipateUserWin():
-  answer = "0"
-  for lis in winnerPos:
-    commonUser = set(userMoves) & set(lis)
+    answer = "0"
+    for lis in winnerPos:
+        commonUser = set(userMoves) & set(lis)
 
-    if len(commonUser) > 1 :
-      for el in lis:
-        if el not in commonUser:
-          for i in moves:
-          if el in moves:
-            answer = el
-            break
-            break
-  return answer
+    if len(commonUser) > 1:
+        for el in lis:
+            if el not in commonUser:
+                if el in moves:
+                    answer = el
+                    break
+                    break
+    return answer
+
 
 def anticipateUserAdvantage():
-  answer = "0"
-  if len(userMoves) < 2:
-    for lis in secondAdvantage:
-      commonUser = set(userMoves) & set(lis)
-      if len(commonUser) > 0 :
-        for el in lis:
-          if el not in commonUser:
-            if el in moves:
-              answer = el
-              break
-              break
-  else:
-    for lis in thirdAdvantage:
-      commonUser = set(userMoves) & set(lis)
+    answer = "0"
+    if len(userMoves) < 2:
+        for lis in secondAdvantage:
+            commonUser = set(userMoves) & set(lis)
+            if len(commonUser) > 0:
+                for el in lis:
+                    if el not in commonUser:
+                        if el in moves:
+                            answer = el
+                            break
+                            break
+    else:
+        for lis in thirdAdvantage:
+            commonUser = set(userMoves) & set(lis)
 
-      if len(commonUser) > 1 :
-        for el in lis:
-          if el not in commonUser:
-            if el in moves:
-              answer = el
-              break
-              break
+            if len(commonUser) > 1:
+                for el in lis:
+                    if el not in commonUser:
+                        if el in moves:
+                            answer = el
+                            break
+                            break
 
-  return answer
+    return answer
+
 
 def anticipateAdvantage():
-  answer = "0"
-  if len(myMoves) < 2:
-    for lis in secondAdvantage:
-      commonEl = set(myMoves) & set(lis)
-      if len(commonEl) > 0 :
-        for el in lis:
-          if el not in commonEl:
-            if el in moves:
-              answer = el
-              break
-              break
-  else:
-    for lis in thirdAdvantage:
-      commonEl = set(myMoves) & set(lis)
+    answer = "0"
+    if len(myMoves) < 2:
+        for lis in secondAdvantage:
+            commonEl = set(myMoves) & set(lis)
+            if len(commonEl) > 0:
+                for el in lis:
+                    if el not in commonEl:
+                        if el in moves:
+                            answer = el
+                            break
+                            break
+    else:
+        for lis in thirdAdvantage:
+            commonEl = set(myMoves) & set(lis)
 
-      if len(commonEl) > 1 :
-        for el in lis:
-          if el not in commonEl:
-            if el in moves:
-              answer = el
-              break
-              break
+            if len(commonEl) > 1:
+                for el in lis:
+                    if el not in commonEl:
+                        if el in moves:
+                            answer = el
+                            break
+                            break
 
-  return answer
-
-      #if ((userMoves[-2] in lis) & (userMoves[-1] in lis) & (myMoves[-1] not in lis)):
-      #  for el in lis:
-      #    if ((el != userMoves[-2]) | (el != userMoves[-1])):
-      #      answer = el
+    return answer
 
 
-
-#switch (move)
 printField()
 
-#print (switch(move))
+
 moveCounter = 0
 matchOver = False
 
 moves = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-userMoves = [];
-myMoves = [];
+userMoves = []
+myMoves = []
 
 moveFirst = input("Who moves first... you or me?: ")
 
 if (moveFirst == "you"):
-  print ("My turn: ")
+    print ("My turn: ")
 
-  if (len(moves) > 0):
+if (len(moves) > 0):
     myMove = think()
 
-  if (myMove in moves):
-      moves.remove(myMove)
-  else:
-      print ("not in list")
+if (myMove in moves):
+    moves.remove(myMove)
+else:
+    print ("not in list")
 
-  populate(myMove, 'o')
-
-  printField()
-
-
-  moveCounter += 1
+populate(myMove, 'o')
+printField()
+moveCounter += 1
 
 catsGame = True
 
 while (moveCounter < 9):
 
-    move = input ("Choose your move: ")
+    move = input("Choose your move: ")
 
     userMoves.append(move)
 
@@ -215,10 +198,7 @@ while (moveCounter < 9):
         break
 
     populate(move, 'x')
-
     printField()
-
-
     moveCounter += 1
 
     if ((field[0][0] == 'x') and (field[0][1] == 'x') and (field[0][2] == 'x')):
@@ -226,39 +206,38 @@ while (moveCounter < 9):
         catsGame = False
         break
     elif ((field[1][0] == 'x') and (field[1][1] == 'x') and (field[1][2] == 'x')):
-      print("you win.")
-      catsGame = False
-      break
+        print("you win.")
+        catsGame = False
+        break
     elif ((field[2][0] == 'x') and (field[2][1] == 'x') and (field[2][2] == 'x')):
-      print("you win.")
-      catsGame = False
-      break
+        print("you win.")
+        catsGame = False
+        break
     elif ((field[0][0] == 'x') and (field[1][0] == 'x') and (field[2][0] == 'x')):
-      print("you win.")
-      catsGame = False
-      break
+        print("you win.")
+        catsGame = False
+        break
     elif ((field[0][1] == 'x') and (field[1][1] == 'x') and (field[2][1] == 'x')):
-      print("you win.")
-      catsGame = False
-      break
+        print("you win.")
+        catsGame = False
+        break
     elif ((field[0][2] == 'x') and (field[1][2] == 'x') and (field[2][2] == 'x')):
-      print("you win.")
-      catsGame = False
-      break
+        print("you win.")
+        catsGame = False
+        break
     elif ((field[0][0] == 'x') and (field[1][1] == 'x') and (field[2][2] == 'x')):
-      print("you win.")
-      catsGame = False
-      break
+        print("you win.")
+        catsGame = False
+        break
     elif ((field[0][2] == 'x') and (field[1][1] == 'x') and (field[2][0] == 'x')):
-      print("you win.")
-      catsGame = False
-      break
+        print("you win.")
+        catsGame = False
+        break
 
     print ("My turn: ")
 
     if len(moves) > 0:
-      myMove = think()
-    #myMove = '2'
+        myMove = think()
 
     if (myMove in moves):
         moves.remove(myMove)
@@ -266,12 +245,8 @@ while (moveCounter < 9):
         print ("not in list")
         break
 
-
     populate(myMove, 'o')
-
     printField()
-
-
     moveCounter += 1
 
     if ((field[0][0] == 'o') and (field[0][1] == 'o') and (field[0][2] == 'o')):
@@ -314,6 +289,6 @@ while (moveCounter < 9):
                                     catsGame = False
                                     break
 if catsGame:
-  print ("cats game...")
+    print ("cats game...")
 else:
-  print("...")
+    print("...")
